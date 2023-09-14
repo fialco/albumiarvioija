b7e7bae656c758109390b31ef820c3e2d243265f
fialco <113672867+fialco@users.noreply.github.com>
2023-09-14 18:24:35 +0300

    testing database integration

--- routes.py
+++ routes.py
@@ -2,6 +2,13 @@ from app import app
 from flask import Flask
 from flask import redirect, render_template, request
 
+
+from db import db
+from sqlalchemy.sql import text
+
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    result = db.session.execute(text("SELECT content FROM messages"))
+    messages = result.fetchall()
+    return render_template("index.html", messages=messages)
