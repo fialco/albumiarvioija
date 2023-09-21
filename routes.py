c6b857297afaef8e58b3fb6157d081246322b0af
fialco <113672867+fialco@users.noreply.github.com>
2023-09-21 14:08:31 +0300

    add user login, register and database fuctionality

--- routes.py
+++ routes.py
@@ -1,7 +1,7 @@
 from app import app
 from flask import Flask
 from flask import redirect, render_template, request
-
+import users
 
 from db import db
 from sqlalchemy.sql import text
@@ -9,6 +9,50 @@ from sqlalchemy.sql import text
 
 @app.route("/")
 def index():
-    result = db.session.execute(text("SELECT content FROM messages"))
-    messages = result.fetchall()
-    return render_template("index.html", messages=messages)
+    #result = db.session.execute(text("SELECT content FROM reviews"))
+    #reviews = result.fetchall()
+    #, reviews=reviews
+    return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if users.login(username, password):
+            return redirect("/")
+        else:
+            return render_template("error.html", message="Väärä tunnus tai salasana")
+    
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    
+    if request.method == "POST":
+        username = request.form["username"]
+        if 1 > len(username) > 20:
+            return render_template("error.html", message="Käyttäjätunnuksen tulisi olla 1-20 merkin välillä")
+
+        password1 = request.form["password1"]
+        password2 = request.form["password2"]
+        if password1 != password2:
+            return render_template("error.html", message="Salasanat eroavat")
+        
+        rank = request.form["rank"]
+        if rank not in ("1", "2", "3"):
+            return render_template("error.html", message="Rooli ei kelpaa")
+        
+        if users.register(username, password1, rank):
+            return redirect("/")
+        else:
+            return render_template("error.html", message="Uuden käyttäjän rekisteröinti ei onnistunut")
+
+    
+@app.route("/logout")
+def logout():
+    users.logout()
+    return redirect("/")
---
+++ users.py
@@ -0,0 +1,43 @@
+import os
+from db import db
+from flask import abort, request, session
+from werkzeug.security import check_password_hash, generate_password_hash
+from sqlalchemy.sql import text
+
+def login(username, password):
+    sql = "SELECT id, password, rank FROM users WHERE username=:username"
+    result = db.session.execute(text(sql), {"username":username})
+    user = result.fetchone()
+    if not user:
+        return False
+    else:
+        if check_password_hash(user.password, password):
+            session["user_id"] = user.id
+            session["username"] = username
+            session["user_rank"] = user.rank
+            session["csrf_token"] = os.urandom(16).hex()
+            return True
+        else:
+            return False
+
+def register(username, password, rank):
+    hash_value = generate_password_hash(password)
+    try:
+        sql = "INSERT INTO users (username, password, rank) VALUES (:username, :password, :rank)"
+        db.session.execute(text(sql), {"username":username, "password":hash_value, "rank":rank})
+        db.session.commit()
+    except:
+        return False
+    return login(username, password)
+
+def user_id():
+    return session.get("user_id", 0)
+
+def logout():
+    del session["user_id"]
+    del session["username"]
+    del session["user_rank"]
+
+def check_csrf():
+    if session["csrf_token"] != request.form["csrf_token"]:
+        abort(403)
