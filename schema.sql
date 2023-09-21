249ef0cead34357e4306572ea53b575389e02566
fialco <113672867+fialco@users.noreply.github.com>
2023-09-21 15:37:28 +0300

    update schema

--- schema.sql
+++ schema.sql
@@ -4,3 +4,26 @@ CREATE TABLE users (
     password TEXT,
     rank INTEGER
 );
+
+CREATE TABLE artists (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    country TEXT,
+    year INTEGER,
+    genre TEXT
+);
+
+CREATE TABLE albums (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    artist_id INTEGER REFERENCES artists,
+    year INTEGER
+);
+
+CREATE TABLE reviews (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users,
+    album_id INTEGER REFERENCES albums,
+    score INTEGER,
+    comment TEXT
+);
