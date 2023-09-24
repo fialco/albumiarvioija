a76a898f80d4ef4051d8f4d1040b61aef3e94ed5
fialco <113672867+fialco@users.noreply.github.com>
2023-09-24 12:22:17 +0300

    Add album review functionality and front page feed

--- artists.py
+++ artists.py
@@ -6,13 +6,28 @@ def all_artists():
     return db.session.execute(text(sql)).fetchall()
 
 def all_albums_from_artist(artist_id):
-    sql = "SELECT id, name, artist_id FROM albums WHERE artist_id=:artist_id ORDER BY name"
+    sql = """SELECT id, name, artist_id FROM albums 
+             WHERE artist_id=:artist_id ORDER BY name"""
     return db.session.execute(text(sql), {"artist_id": artist_id}).fetchall()
 
+def all_reviews_for_album(album_id):
+    sql = "SELECT id, user_id, score, comment FROM reviews WHERE album_id=:album_id"
+    return db.session.execute(text(sql), {"album_id": album_id}).fetchall()
+
+def all_reviews_reversed():
+    sql = "SELECT id, user_id, score, comment FROM reviews ORDER BY id DESC"
+    return db.session.execute(text(sql)).fetchall()
+
 def artist_info(artist_id):
-    sql = "SELECT * FROM artists WHERE id=:artist_id"
+    sql = "SELECT name, country, year, genre FROM artists WHERE id=:artist_id"
     return db.session.execute(text(sql), {"artist_id": artist_id}).fetchone()
 
 def album_info(album_id):
-    sql = "SELECT * FROM albums WHERE id=:album_id"
+    sql = "SELECT name, artist_id, year FROM albums WHERE id=:album_id"
     return db.session.execute(text(sql), {"album_id": album_id}).fetchone()
+
+def add_review(user_id, album_id, score, comment):
+    sql = """INSERT INTO reviews (user_id, album_id, score, comment) 
+             VALUES (:user_id, :album_id, :score, :comment)"""
+    db.session.execute(text(sql), {"user_id":user_id, "album_id":album_id, "score":score, "comment":comment})
+    db.session.commit()
--- routes.py
+++ routes.py
@@ -4,35 +4,50 @@ from flask import redirect, render_template, request
 import users
 import artists
 
-from db import db
-from sqlalchemy.sql import text
-
-
 @app.route("/")
 def index():
-    #result = db.session.execute(text("SELECT content FROM reviews"))
-    #reviews = result.fetchall()
-    #, reviews=reviews
-    return render_template("index.html")
+    reviews = artists.all_reviews_reversed()
+    return render_template("index.html", reviews=reviews)
 
 @app.route("/browse")
 def browse():
-    #result = db.session.execute(text("SELECT content FROM reviews"))
-    #reviews = result.fetchall()
-    #, reviews=reviews
     return render_template("browse.html", artists=artists.all_artists())
 
 @app.route("/artists/<int:artist_id>")
 def artist_page(artist_id):
     artist = artists.artist_info(artist_id)
     albums = artists.all_albums_from_artist(artist_id)
-    return render_template("artist.html", id=artist_id, name=artist.name, country=artist.country, year=artist.year, genre=artist.genre, albums=albums)
+
+    return render_template("artist.html", id=artist_id, name=artist.name, country=artist.country, 
+                           year=artist.year, genre=artist.genre, albums=albums)
 
 @app.route("/albums/<int:album_id>")
 def album_page(album_id):
     album = artists.album_info(album_id)
     artist = artists.artist_info(album.artist_id)
-    return render_template("album.html", id=album_id, name=album.name, artist_id=album.artist_id, year=album.year, artist_name=artist.name)
+    reviews = artists.all_reviews_for_album(album_id)
+
+    return render_template("album.html", id=album_id, name=album.name, artist_id=album.artist_id, 
+                           year=album.year, artist_name=artist.name, reviews=reviews)
+
+@app.route("/review/", methods=["POST"])
+def review():
+    users.check_csrf()
+    album_id = request.form["album_id"]
+
+    score = int(request.form["score"])
+    if score < 1 or 5 < score:
+        return render_template("error.html", message="Pistemäärä ei kelpaa")
+
+    comment = request.form["comment"]
+    if len(comment) > 1000:
+        return render_template("error.html", message="Kommentti on liian pitkä")
+    if comment == "":
+        return render_template("error.html", message="Kommenttikenttä on tyhjä")
+
+    artists.add_review(users.user_id(), album_id, score, comment)
+    return redirect("/albums/"+str(album_id))
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -54,13 +69,13 @@ def register():
     
     if request.method == "POST":
         username = request.form["username"]
-        if 1 > len(username) > 20:
+        if len(username) < 1 or 20 < len(username):
             return render_template("error.html", message="Käyttäjätunnuksen tulisi olla 1-20 merkin välillä")
 
         password1 = request.form["password1"]
         password2 = request.form["password2"]
         if password1 != password2:
-            return render_template("error.html", message="Salasanat eroavat")
+            return render_template("error.html", message="Salasanat eivät ole samat")
         
         rank = request.form["rank"]
         if rank not in ("1", "2", "3"):
@@ -71,7 +86,6 @@ def register():
         else:
             return render_template("error.html", message="Uuden käyttäjän rekisteröinti ei onnistunut")
 
-    
 @app.route("/logout")
 def logout():
     users.logout()
